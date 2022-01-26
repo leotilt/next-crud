8d9f48972d29c3c02bfecba9395fd347e23f471a
Leonardo Oliveira <71794931+leotilt@users.noreply.github.com>
2022-01-26 15:33:34 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Botao from "../components/Botao";
 import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
@@ -25,6 +26,9 @@ export default function Home() {
     `}
     >
       <Layout titulo="Cadastro Simples">
+        <div className="flex justify-end ">
+          <Botao className="mb-4"> Novo Cliente </Botao>
+        </div>
         <Tabela
           clientes={clientes}
           clienteSelecionado={clienteSelecionado}
