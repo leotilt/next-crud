64e30791542582bdcafd26aff92f1505a006e960
Leonardo Oliveira <71794931+leotilt@users.noreply.github.com>
2022-01-25 22:53:24 -0300

    create

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
+import Layout from "../components/Layout";
+
 export default function Home() {
   return (
-    <div className={`flex h-screen justify-center items-center`}>
-      <span>Texto</span>
+    <div
+      className={`flex h-screen justify-center items-center
+     bg-gradient-to-r from-purple-500 to to-blue-700 text-white
+    `}
+    >
+      <Layout titulo="Cadastro Simples">
+        <span>Conteudo</span>
+      </Layout>
     </div>
-  )
+  );
 }
